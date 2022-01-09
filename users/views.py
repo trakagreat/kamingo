95b7042328f440d01530ff59374fc1918969686b
Trivendra <trivendra.rahi05@gmail.com>
2022-01-09 22:27:57 +0530

    evening work

--- users/views.py
+++ users/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from .forms import RegisterUserForm
+from django.contrib.auth import authenticate, login, logout
 
 
 # Create your views here.
 
 
 def login_page(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+
     return render(request, 'users/login.html')
 
 
