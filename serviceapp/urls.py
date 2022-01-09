c52cad7fa65e98684c88a246496d3fa41a9e80c7
Trivendra <trivendra.rahi05@gmail.com>
2022-01-09 10:53:32 +0530

    added save of service from to the database

--- serviceapp/urls.py
+++ serviceapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path( '', views.front_page),
-    path('service-form', views.service_form)
+    path('', views.front_page),
+    path('service-form', views.service_form_view, name='service_form_url')
 ]
--- serviceapp/views.py
+++ serviceapp/views.py
@@ -6,10 +6,15 @@ from .models import ServiceModel
 # Create your views here.
 # Create your views here.
 def front_page(request):
-    return render( request, 'base.html')
+    return render(request, 'base.html')
 
-def service_form(request):
-    form = ServiceForm()
+
+def service_form_view(request):
+    service_form = ServiceForm()
+    if request.method == "POST":
+        service = ServiceForm(request.POST)
+        if service.is_valid():
+            new_service = service.save(commit=True)
     return render(request, 'serviceapp/service_form.html', {
-        "form": form
+        "form": service_form
     })
